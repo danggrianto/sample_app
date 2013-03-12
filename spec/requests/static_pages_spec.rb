3cf85e56f58793b54c08cd53aa73cd577965a216
Daniel Anggrianto <d.anggrianto@gmail.com>
2013-03-11 22:03:30 -0400

    adding full title controller and some guard related file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) { "RoR Sample App"  }
 	describe "Home Page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
-		it "should have the right title" do
+		it "should have base title" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-				:text => "RoR Sample App | Home")
+				:text => "#{base_title}")
+		end
+
+		it "should not have custom title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title',
+				:text => "| Home")
 		end
 	end
 
@@ -23,7 +30,7 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title',
-				:text => "RoR Sample App | Help")
+				:text => "#{base_title} | Help")
 		end
 	end
 
@@ -36,8 +43,22 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
-				:text => "RoR Sample App | About Us")
+				:text => "#{base_title} | About Us")
 		end
+	end
+
+	describe "Contact" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+				:text => "#{base_title} | Contact")
+		end
+
 	end
 	
 end
