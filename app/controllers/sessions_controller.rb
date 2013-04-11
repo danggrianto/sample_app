6b9e8d11bc32c450b7e3cd7975eae4077c4b116e
Daniel Anggrianto <d.anggrianto@gmail.com>
2013-04-10 22:24:26 -0400

    sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+	def new		
+		# signin
+	end
+
+	def create
+		# create new session
+		user = User.find_by_email(params[:email].downcase)
+		if(user && user.authenticate(params[:password]))
+			session[:user] = user.id
+			sign_in user
+			redirect_to user
+		else
+			# create error
+			flash.now[:error] = "Invalid email/password combinaion"
+			render 'new'
+		end
+
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+end
