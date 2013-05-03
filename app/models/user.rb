d43e64c321ad7eec94690184c5f3496797227157
Daniel Anggrianto <d.anggrianto@gmail.com>
2013-05-02 22:30:26 -0400

    10.2.1 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,15 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   #make all lower case email
   # before_save { |user| user.email = email.downcase }
